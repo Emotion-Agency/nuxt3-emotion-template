b1aedeac94e3d3749ddd0bac4f8c7dbda4d2a366
letmewin22 <lkosteckiy5@gmail.com>
2023-07-18 10:50:49 +0300

    feat: add post css plugin to group media

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
+import PostCSSCombineMediaQuery from 'postcss-combine-media-query'
+
 export default defineNuxtConfig({
   css: ['@/assets/styles/index.scss'],
+
+  vite: {
+    css: {
+      postcss: {
+        plugins: [PostCSSCombineMediaQuery],
+      },
+    },
+  },
 
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
